ebcd9634b7519ca7ff0cc2ce853de033cb1e5c3f
lsewilson <laurawilson@hotmail.co.uk>
2016-10-19 16:24:33 +0100

    tests for brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,25 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  describe("Aged Brie", function() {
+    it("should be able to change sell_in property", function() {
+      items = [ new Item("Aged Brie", 1, 10) ];
+      update_quality();
+      expect(items[0].sell_in).toEqual(0);
+    });
+    it("should have a maximum quality of 50", function() {
+      items = [ new Item("Aged Brie", 0, 50) ];
+      update_quality();
+      expect(items[0].quality).toEqual(50);
+    });
+    it("should increase quality twice as fast when sell_in< 0", function() {
+      items = [ new Item("Aged Brie", 0, 0) ];
+      update_quality();
+      expect(items[0].quality).toEqual(2);
+    });
+    it("should should increase in quality with decrease in sell_in", function() {
+      items = [ new Item("Aged Brie", 1, 10) ];
+      update_quality();
+      expect(items[0].quality).toEqual(11);
+    });
   });
-
 });
