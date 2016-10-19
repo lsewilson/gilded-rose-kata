030025f69594aac294a455f11e98a5d6d6987a01
lsewilson <laurawilson@hotmail.co.uk>
2016-10-19 19:40:43 +0100

    refactors backstage passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,6 +13,8 @@ function update_quality() {
       _update_sulfuras_quality();
     } else if (items[i].name == "Aged Brie") {
       _update_brie_quality(items[i]);
+    } else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+      _update_backstage_pass_quality(items[i]);
     }
     if (items[i].sell_in < 0) {
       if (items[i].name == "Sulfuras, Hand of Ragnaros") {
@@ -21,49 +23,6 @@ function update_quality() {
         _update_brie_quality(items[i]);
       }
     }
-  //   if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //     if (items[i].quality > 0) {
-  //       if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //         items[i].quality = items[i].quality - 1
-  //       }
-  //     }
-  //   } else {
-  //     if (items[i].quality < 50) {
-  //       items[i].quality = items[i].quality + 1
-  //       if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-  //         if (items[i].sell_in < 11) {
-  //           if (items[i].quality < 50) {
-  //             items[i].quality = items[i].quality + 1
-  //           }
-  //         }
-  //         if (items[i].sell_in < 6) {
-  //           if (items[i].quality < 50) {
-  //             items[i].quality = items[i].quality + 1
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //     items[i].sell_in = items[i].sell_in - 1;
-  //   }
-  //   if (items[i].sell_in < 0) {
-  //     if (items[i].name != 'Aged Brie') {
-  //       if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //         if (items[i].quality > 0) {
-  //           if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //             items[i].quality = items[i].quality - 1
-  //           }
-  //         }
-  //       } else {
-  //         items[i].quality = items[i].quality - items[i].quality
-  //       }
-  //     } else {
-  //       if (items[i].quality < 50) {
-  //         items[i].quality = items[i].quality + 1
-  //       }
-  //     }
-  //   }
   }
 }
 
@@ -76,5 +35,17 @@ function _update_sulfuras_quality() {}
 function _update_brie_quality(item) {
   if (item.quality < 50) {
     item.quality++;
+  }
+}
+
+function _update_backstage_pass_quality(item) {
+  if (item.sell_in >= 10) {
+    item.quality++;
+  } else if (item.sell_in >= 5) {
+    item.quality += 2;
+  } else if (item.sell_in >= 0) {
+    item.quality += 3;
+  } else {
+    item.quality = 0;
   }
 }
