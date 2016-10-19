245d88dbe0ab0f490f05bc2cf6f8e0730714a744
lsewilson <laurawilson@hotmail.co.uk>
2016-10-19 19:55:06 +0100

    initial refactor complete

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -8,21 +8,15 @@ var items = [];
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
+    _update_quality(items[i]);
     _update_sell_in(items[i]);
-    if (items[i].name == "Sulfuras, Hand of Ragnaros") {
-      _update_sulfuras_quality();
-    } else if (items[i].name == "Aged Brie") {
-      _update_brie_quality(items[i]);
-    } else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-      _update_backstage_pass_quality(items[i]);
-    } else {
-      _update_regular_quality(items[i]);
-    }
     if (items[i].sell_in < 0) {
       if (items[i].name == "Sulfuras, Hand of Ragnaros") {
         _update_sulfuras_quality();
       } else if (items[i].name == "Aged Brie") {
         _update_brie_quality(items[i]);
+      } else if (items[i].name == "Conjured Mana Cake") {
+        _update_conjured_quality(items[i]);
       } else {
         _update_regular_quality(items[i]);
       }
@@ -34,6 +28,20 @@ function _update_sell_in(item) {
   if (item.name != "Sulfuras, Hand of Ragnaros") {item.sell_in--;}
 }
 
+function _update_quality(item) {
+  if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+    _update_backstage_pass_quality(item);
+  } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+    _update_sulfuras_quality(item);
+  } else if (item.name == "Aged Brie") {
+    _update_brie_quality(item);
+  } else if (item.name == "Conjured Mana Cake") {
+    _update_conjured_quality(item);
+  } else {
+    _update_regular_quality(item);
+  }
+}
+
 function _update_sulfuras_quality() {}
 
 function _update_brie_quality(item) {
@@ -43,11 +51,11 @@ function _update_brie_quality(item) {
 }
 
 function _update_backstage_pass_quality(item) {
-  if (item.sell_in >= 10) {
+  if (item.sell_in > 10) {
     item.quality++;
-  } else if (item.sell_in >= 5) {
+  } else if (item.sell_in > 5) {
     item.quality += 2;
-  } else if (item.sell_in >= 0) {
+  } else if (item.sell_in > 0) {
     item.quality += 3;
   } else {
     item.quality = 0;
@@ -57,5 +65,11 @@ function _update_backstage_pass_quality(item) {
 function _update_regular_quality(item) {
   if (item.quality > 0) {
     item.quality--;
+  }
+}
+
+function _update_conjured_quality(item) {
+  if (item.quality > 0) {
+    item.quality -= 2;
   }
 }
